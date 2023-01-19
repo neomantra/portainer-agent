e444c20bf4261c7096d0eb8991f5f7136c9cbac1
Evan Wies <evan@neomantra.net>
2023-01-19 10:34:23 -0500

    feat(nomad/edge): debug log nomadOperation request URL

--- http/handler/nomadproxy/nomad_operation.go
+++ http/handler/nomadproxy/nomad_operation.go
@@ -6,9 +6,14 @@ import (
 	"github.com/portainer/agent"
 
 	httperror "github.com/portainer/libhttp/error"
+	"github.com/rs/zerolog/log"
 )
 
 func (handler *Handler) nomadOperation(rw http.ResponseWriter, request *http.Request) *httperror.HandlerError {
+	log.Debug().
+		Str("nomad_request_url", request.URL.String()).
+		Msg("nomadOperation")
+
 	request.Header.Set(agent.HTTPNomadTokenHeaderName, handler.nomadConfig.NomadToken)
 	http.StripPrefix("/nomad", handler.nomadProxy).ServeHTTP(rw, request)
 
